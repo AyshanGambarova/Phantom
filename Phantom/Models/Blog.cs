c424845e10af4f08298dc3d58cf52b9e7688c81d
User <User@WINDOWS-1TDC0VN>
2022-03-03 21:44:27 +0400

    .

---
+++ Phantom/Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phantom.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Img { get; set; }
+        public string Text { get; set; }
+
+    }
+}
